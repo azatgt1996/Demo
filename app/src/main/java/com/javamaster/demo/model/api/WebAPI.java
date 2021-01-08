a7c90ad7430590b685ed8ccedbe58a7d4aef547d
Azat <azatgt1996@yandex.ru>
2021-01-08 20:15:37 +0300

    08/01/21 delete comments for testing

--- app/src/main/java/com/javamaster/demo/model/api/WebAPI.java
+++ app/src/main/java/com/javamaster/demo/model/api/WebAPI.java
@@ -70,17 +70,6 @@ public class WebAPI implements API {
 
             JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, jsonObject, successListener, errorListener);
             mRequestQueue.add(request);
-            // for testing
-//            {
-//                JSONObject response = new JSONObject();
-//                response.put("name", "Maxim Petrov");
-//                response.put("login", "azatgt");
-//                response.put("email", "azat12@mail.com");
-//                response.put("token", "sf45d4f3sert.1842312dfg12ki.ui843x21w");
-//
-//                User user = User.getUser(response);
-//                listener.onLogin(user);
-//            }
         }
         catch (JSONException e) {
             Toast.makeText(mApplication, "JSON exception", Toast.LENGTH_LONG).show();
@@ -120,13 +109,6 @@ public class WebAPI implements API {
 
             JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, jsonObject, successListener, errorListener);
             mRequestQueue.add(request);
-            // for testing
-//            {
-//                JSONObject response = new JSONObject();
-//                response.put("message", "Visit your email to activate");
-//                String mes = response.getString("message");
-//                listener.onRegistered(mes);
-//            }
         }
         catch (JSONException e) {
             Toast.makeText(mApplication, "JSON exception", Toast.LENGTH_LONG).show();
@@ -164,13 +146,6 @@ public class WebAPI implements API {
 
             JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, jsonObject, successListener, errorListener);
             mRequestQueue.add(request);
-            // for testing
-//            {
-//                JSONObject response = new JSONObject();
-//                response.put("message", "Visit your email to activate");
-//                String mes = response.getString("message");
-//                listener.onRecovered(mes);
-//            }
         }
         catch (JSONException e) {
             Toast.makeText(mApplication, "JSON exception", Toast.LENGTH_LONG).show();
@@ -181,7 +156,6 @@ public class WebAPI implements API {
     public void loadPhones(final APIListener listener) {
         String url = BASE_URL + "api/phones";
 
-//    try {
         Response.Listener<JSONArray> successListener = new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
@@ -214,30 +188,6 @@ public class WebAPI implements API {
             }
         };
         mRequestQueue.add(request);
-            // for testing
-//            {
-//                JSONArray jsonArray = new JSONArray();
-//                JSONObject p1 = new JSONObject();
-//                p1.put("id", 1);
-//                p1.put("phoneNumber", "255 96 33");
-//                JSONObject p2 = new JSONObject();
-//                p2.put("id", 2);
-//                p2.put("phoneNumber", "276-88-99");
-//                JSONObject p3 = new JSONObject();
-//                p3.put("id", 3);
-//                p3.put("phoneNumber", "8 908 999 66 33");
-//
-//                jsonArray.put(p1);
-//                jsonArray.put(p2);
-//                jsonArray.put(p3);
-//                List<Phone> phones = Phone.getPhones(jsonArray);
-//                if (listener != null) {
-//                    listener.onPhonesLoaded(phones);
-//                }
-//            }
-//        } catch (JSONException e) {
-//            Toast.makeText(mApplication, "JSON exception", Toast.LENGTH_LONG).show();
-//        }
     }
 
     @Override
@@ -280,16 +230,6 @@ public class WebAPI implements API {
                 }
             };
             mRequestQueue.add(request);
-            // for testing
-//            {
-//                JSONObject response = new JSONObject();
-//                response.put("message", "Phone successfully added");
-//                response.put("id", 100);
-//
-//                String mes = response.getString("message");
-//                int phoneId = response.getInt("id");
-//                listener.onPhoneAdded(mes, phoneId);
-//            }
         }
         catch (JSONException e) {
             Toast.makeText(mApplication, "JSON exception", Toast.LENGTH_LONG).show();
@@ -300,57 +240,43 @@ public class WebAPI implements API {
     public void deletePhone(int idPhone, final APIListener listener) {
         String url = BASE_URL + "api/phones/" + idPhone;
 
-//        try {
-            Response.Listener<JSONObject> successListener = new Response.Listener<JSONObject>() {
-                @Override
-                public void onResponse(JSONObject response) {
-                    try {
-                        String mes = response.getString("message");
-                        listener.onPhoneDeleted(mes);
-                    }
-                    catch (JSONException e) {
-                        Toast.makeText(mApplication, "JSON exception", Toast.LENGTH_LONG).show();
-                    }
+        Response.Listener<JSONObject> successListener = new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String mes = response.getString("message");
+                    listener.onPhoneDeleted(mes);
                 }
-            };
+                catch (JSONException e) {
+                    Toast.makeText(mApplication, "JSON exception", Toast.LENGTH_LONG).show();
+                }
+            }
+        };
 
-            Response.ErrorListener errorListener = new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    showErrorMessage(error);
-                }
-            };
+        Response.ErrorListener errorListener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                showErrorMessage(error);
+            }
+        };
 
-            JsonObjectRequest request = new JsonObjectRequest(Request.Method.DELETE, url, null, successListener, errorListener) {
-                @Override
-                public Map<String, String> getHeaders() {
-                    Map<String, String> headers = new HashMap<>();
-                    headers.put("Content-Type", "application/json");
-                    headers.put("Accept", "application/json");
-                    headers.put("Authorization", "Bearer " + mModel.getUser().getToken());
-                    return headers;
-                }
-            };
-            mRequestQueue.add(request);
-            // for testing
-//            {
-//                JSONObject response = new JSONObject();
-//                response.put("message", "Phone successfully deleted");
-//
-//                String mes = response.getString("message");
-//                listener.onPhoneDeleted(mes);
-//            }
-//        }
-//        catch (JSONException e) {
-//            Toast.makeText(mApplication, "JSON exception", Toast.LENGTH_LONG).show();
-//        }
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.DELETE, url, null, successListener, errorListener) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/json");
+                headers.put("Accept", "application/json");
+                headers.put("Authorization", "Bearer " + mModel.getUser().getToken());
+                return headers;
+            }
+        };
+        mRequestQueue.add(request);
     }
 
     @Override
     public void deleteAllPhones(final APIListener listener) {
         String url = BASE_URL + "api/phones" ;
 
-//        try {
         Response.Listener<JSONObject> successListener = new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
@@ -382,18 +308,6 @@ public class WebAPI implements API {
             }
         };
         mRequestQueue.add(request);
-            // for testing
-//            {
-//                JSONObject response = new JSONObject();
-//                response.put("message", "All phones successfully deleted");
-//
-//                String mes = response.getString("message");
-//                listener.onAllPhonesDeleted(mes);
-//            }
-//        }
-//        catch (JSONException e) {
-//            Toast.makeText(mApplication, "JSON exception", Toast.LENGTH_LONG).show();
-//        }
     }
 
     @Override
@@ -435,14 +349,6 @@ public class WebAPI implements API {
                 }
             };
             mRequestQueue.add(request);
-            // for testing
-//            {
-//                JSONObject response = new JSONObject();
-//                response.put("message", "Phone successfully updated");
-//
-//                String mes = response.getString("message");
-//                listener.onPhoneUpdated(mes);
-//            }
         }
         catch (JSONException e) {
             Toast.makeText(mApplication, "JSON exception", Toast.LENGTH_LONG).show();
